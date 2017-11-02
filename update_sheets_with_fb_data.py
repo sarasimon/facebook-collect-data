0638ae60651a61be422cd57abbdc221d544053e2
Prachi <prachisr@thoughtworks.com>
2017-11-02 12:29:46 +0100

    Removed unused code from update_sheets

--- update_sheets_with_fb_data.py
+++ update_sheets_with_fb_data.py
@@ -4,8 +4,6 @@ from oauth2client import client
 from oauth2client import tools
 from argparse import ArgumentParser
 import os
-import itertools
-import json
 
 from import_feed import search_active_users
 
@@ -14,32 +12,6 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 APPLICATION_NAME = "my project"
 
 range_ = "Fb_data_sheet!A1"
-
-
-def flatten_list(list_to_flatten):
-    return [item for sublist in list_to_flatten for item in sublist]
-
-
-def get_the_list_of_volunteer_for_period(volunteer):
-    volunteer_names_list = []
-    volunteer_frequency_list = []
-    for volunteer in volunteer['persons']:
-        for key, value in volunteer.items():
-            volunteer_names_list.append(key)
-            volunteer_frequency_list.append(value)
-    return [volunteer_names_list, volunteer_frequency_list]
-
-
-def get_sheet_data(frame_size):
-    group_volunteers = group_by_count(group_by_frame_size(list(extract_volunteers_from_csv()), frame_size))
-    volunteers_list = flatten_list(list(map(get_the_list_of_volunteer_for_period, group_volunteers)))
-
-    list_of_dates = flatten_list([[volunteer['weekRange'], 'frequency'] for volunteer in group_volunteers])
-    final_list = [list_of_dates, []]
-
-    for vol_list in list(itertools.zip_longest(*volunteers_list)):
-        final_list.append(list(vol_list))
-    return final_list
 
 
 def writeToFile(service, data, spreadsheet_id):
@@ -63,7 +35,7 @@ def get_credentials(flags):
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.esperanca.data.json')
+                                   'sheets.googleapis.esperanca.fb.data.json')
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -90,7 +62,6 @@ def main():
     credentials = get_credentials(args)
     service = discovery.build('sheets', 'v4', credentials=credentials)
     data_in_dictionary_format = search_active_users(args.since, args.until)
-    print(type(data_in_dictionary_format))
     data_in_list_of_lists_format = get_fb_data_for_sheets(data_in_dictionary_format)
 
     writeToFile(service, data_in_list_of_lists_format, args.sheet_id)
